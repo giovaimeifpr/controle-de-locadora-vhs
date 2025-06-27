d7955970b46ef6a27ea0315e93dff50bc32bf386
giovaime <giovaime@gmail.com>
2025-06-27 16:41:48 -0300

    Projeto Spring inicial

---
+++ src/main/java/br/edu/ifpr/foz/gestao_fitas_vhs/GestaoFitasVhsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.foz.gestao_fitas_vhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFitasVhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFitasVhsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpr/foz/gestao_fitas_vhs/GestaoFitasVhsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpr.foz.gestao_fitas_vhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFitasVhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
